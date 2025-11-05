53d9175d6b4da7e8d1d2cc83675fd911c11e87f3
melodyrqu-ui <melodyr.qu@gmail.com>
2025-11-05 14:30:12 -0500

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 // --- 1. Database connection ---
-$servername = "db.fr-pari1.bengt.wasmernet.com";
+$servername = "localhost";
 $username = "ba367aaa7dac80008fdb8166dd25";      // change if needed
 $password = "0690ba36-7aaa-7f98-8000-73a0e9e88b08";          // change if needed
 $dbname = "myDatabase"; // change to your DB name
@@ -72,4 +72,4 @@ $result = $conn->query($sql);
 </body>
 </html>
 
-wasmer deploy
+$wasmer run .
